4e7011c95b76f82e3903ddd70c1acffc6663e364
Siyuan Gong <contentgong@gmail.com>
2018-11-30 08:00:47 -0500

    Update add-two-numbers.py

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -5,21 +5,22 @@
 #         self.next = None
 
 class Solution:
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
         eval_ = lambda l: 0 if l is None else l.val
         next_ = lambda l: None if l is None else l.next
-        curr = dummy_head = ListNode(0)  
-        carry = 0 # Initiated to carry from one iteration to another
+        carry = 0
+        curr = head = ListNode(carry)  
         while l1 or l2 or carry:
-            sum_ = eval_(l1) + eval_(l2) + carry
-            curr.next = ListNode(sum_ % 10)
-            curr = curr.next
+            curr.next = ListNode(carry)
+            sum_ = eval_(l1) + eval_(l2) + curr.val
+            curr.val = sum_ % 10
             l1 = next_(l1)
             l2 = next_(l2)
             carry = sum_ // 10
-        return dummy_head.next
+            curr = curr.next
+        return head
