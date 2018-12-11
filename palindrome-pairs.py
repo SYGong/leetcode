4dcfd51da540481a29853998d5567bed2b824b2d
Siyuan Gong <contentgong@gmail.com>
2018-12-10 22:24:30 -0500

    Update palindrome-pairs.py
    Time Limit Exceeded

--- palindrome-pairs.py
+++ palindrome-pairs.py
@@ -7,7 +7,26 @@ class Solution:
         pairs = []
         for i, v in enumerate(words):
             for j, w in enumerate(words):
-                if (i != j 
-                    and v + w == w[::-1] + v[::-1]):
+                if i != j and self.isPair(v, w):
                     pairs.append([i, j])
         return pairs
+    
+    def isPair(self, a, b):
+        if len(a) <= len(b):
+            for i in range(len(a)):
+                if a[i] != b[-i - 1]:
+                    return False
+            b = b[:len(b) - len(a)]
+            for i in range(len(b) // 2):
+                if b[i] != b[-i -1]:
+                    return False
+            return True
+        else:
+            for i in range(len(b)):
+                if a[i] != b[-i - 1]:
+                    return False
+            a = a[len(b):]
+            for i in range(len(a) // 2):
+                if a[i] != a[-i -1]:
+                    return False
+            return True
