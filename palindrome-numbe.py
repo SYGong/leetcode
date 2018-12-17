5df9176f68fc70d842d01bbeaad70c43dba8956f
Siyuan Gong <contentgong@gmail.com>
2018-12-16 19:41:25 -0500

    Create palindrome-numbe.py

---
+++ palindrome-numbe.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x == 0:
+            return True
+        if x < 0 or x % 10 == 0:
+            return False
+        rev_x = 0
+        while x > rev_x:
+            rev_x *= 10 
+            rev_x += x % 10
+            x //= 10
+        return x == rev_x or x == rev_x // 10
