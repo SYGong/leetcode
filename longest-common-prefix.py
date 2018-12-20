05626adff7be918a51a83e0a2e0963afb876e596
Siyuan Gong <contentgong@gmail.com>
2018-12-20 11:50:36 -0500

    Update longest-common-prefix.py

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -4,13 +4,10 @@ class Solution:
         :type strs: List[str]
         :rtype: str
         """
-        prefix = ''
         if len(strs) == 0:
-            return prefix
-        min_len = min(map(len, strs))
-        for i in range(min_len):
-            for j in range(len(strs) - 1):
-                if strs[j][i] != strs[j + 1][i]:
-                    return prefix
-            prefix += strs[0][i]
-        return prefix
+            return ''
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if len(strs[j]) <= i or strs[0][i] != strs[j][i]:
+                    return strs[0][:i]
+        return strs[0]
