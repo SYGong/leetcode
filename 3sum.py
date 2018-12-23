ffdca9de35613bbbef52689cdc002c69e4ea0032
Siyuan Gong <contentgong@gmail.com>
2018-12-23 18:40:46 -0500

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -4,12 +4,26 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        nums = sorted(nums)
         triplets = []
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
-                    triplet =[nums[i], nums[j], nums[k]]
-                    if sum(triplet) == 0:
-                        triplets.append(triplet)
-            
-        return triplets 
+            if i == 0 or nums[i] != nums[i - 1]:
+                two_sum = -nums[i]
+                incre = i + 1
+                decre = len(nums) - 1 
+                while incre < decre and 2 * nums[incre] <= two_sum and 2 * nums[decre] >= two_sum:
+                    if nums[incre] + nums[decre] == two_sum:
+                        triplets.append([nums[i], nums[incre], nums[decre]])
+                        if nums[incre] == nums[decre]:
+                            break
+                        incre += 1
+                        decre -= 1
+                        while incre < decre and nums[incre - 1] == nums[incre]:
+                            incre += 1
+                        while incre < decre and nums[decre + 1] == nums[decre]:
+                            decre -= 1
+                    elif nums[incre] + nums[decre] > two_sum:
+                        decre -= 1
+                    else:
+                        incre += 1
+        return triplets
