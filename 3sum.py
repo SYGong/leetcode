1a2f430660bf938a8316c0e0bfd0f5e2de021e99
Siyuan Gong <contentgong@gmail.com>
2018-12-23 21:41:45 -0500

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -1,27 +1,35 @@
+from collections import Counter
+
 class Solution:
     def threeSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums = sorted(nums)
+        TARGET = 0
+        num_freq = Counter(nums)
+        nums = sorted(num_freq)
+        # unique numbers in ascending order
+
         triplets = []
-        for i in range(len(nums)):
-            if i == 0 or nums[i] != nums[i - 1]:
-                two_sum = -nums[i]
+        for i, v in enumerate(nums)):
+            if num_freq[v] >= 2:
+                if v * 3 == TARGET and num_freq[v] >= 3:
+                    triplets.appned([v] * 3)
+                    break
+                else:
+                    complement = TARGET - 2 * v
+                    if complement in num_freq:
+                        triplets.append([v] * 2 + [complement])
+            elif v < 0:
+                two_sum = TARGET - v
                 incre = i + 1
                 decre = len(nums) - 1 
                 while incre < decre and 2 * nums[incre] <= two_sum and 2 * nums[decre] >= two_sum:
                     if nums[incre] + nums[decre] == two_sum:
                         triplets.append([nums[i], nums[incre], nums[decre]])
-                        if nums[incre] == nums[decre]:
-                            break
                         incre += 1
                         decre -= 1
-                        while incre < decre and nums[incre - 1] == nums[incre]:
-                            incre += 1
-                        while incre < decre and nums[decre + 1] == nums[decre]:
-                            decre -= 1
                     elif nums[incre] + nums[decre] > two_sum:
                         decre -= 1
                     else:
