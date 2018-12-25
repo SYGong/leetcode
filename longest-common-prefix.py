def482dc36ab892718797f3610d5a1242a24010d
Siyuan Gong <contentgong@gmail.com>
2018-12-25 01:24:22 -0500

    Update longest-common-prefix.py
    Because it is so fast to compare two strings by == or other build in methods like startswith
    It took advantage of string compare and start with longer prefix

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -4,10 +4,49 @@ class Solution:
         :type strs: List[str]
         :rtype: str
         """
-        if len(strs) == 0:
+        if not strs:
             return ''
-        for i in range(len(strs[0])):
-            for j in range(1, len(strs)):
-                if len(strs[j]) <= i or strs[0][i] != strs[j][i]:
-                    return strs[0][:i]
-        return strs[0]
+        # j=999
+        # for each in strs:
+        #     a = len(each)
+        #     if a<j:
+        #         j=a
+        # b=0
+        # while b < len(strs):
+        #     a=strs[0][0:j]
+        #     if a in strs[b][0:j]:
+        #         b+=1
+        #     else:
+        #         b+=1
+        #         j=j-1
+        #     if j==0:
+        #         return ''
+        # return a
+                
+        # a=0
+        # while a<=j:
+        #     for x in range(len(strs)):
+        #         result.append(strs[x][a])
+        #         if strs[a][x] in result:
+        #             continue
+        #         else:
+        #             result.pop()
+        #             return result
+        #     a+=1
+        # return result
+        commonPrefix = strs[0]
+        for string in strs:
+            commonPrefix = self.findPrefix(commonPrefix,string)
+
+        return commonPrefix
+
+    def findPrefix(self,prefix, string):
+        while prefix is not "":
+            
+            if string.startswith(prefix):
+                
+                return prefix
+            else:
+                prefix = prefix[:-1]
+
+        return ""
