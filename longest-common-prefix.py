c6fdc4c81723b84ea3ef2e2124fc3221f16c2079
Siyuan Gong <contentgong@gmail.com>
2018-12-25 01:34:16 -0500

    Update longest-common-prefix.py
    min max has the largeest distance. In terms of trie, they seperate wide after LCP

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -1,52 +1,11 @@
 class Solution:
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        if not strs:
-            return ''
-        # j=999
-        # for each in strs:
-        #     a = len(each)
-        #     if a<j:
-        #         j=a
-        # b=0
-        # while b < len(strs):
-        #     a=strs[0][0:j]
-        #     if a in strs[b][0:j]:
-        #         b+=1
-        #     else:
-        #         b+=1
-        #         j=j-1
-        #     if j==0:
-        #         return ''
-        # return a
-                
-        # a=0
-        # while a<=j:
-        #     for x in range(len(strs)):
-        #         result.append(strs[x][a])
-        #         if strs[a][x] in result:
-        #             continue
-        #         else:
-        #             result.pop()
-        #             return result
-        #     a+=1
-        # return result
-        commonPrefix = strs[0]
-        for string in strs:
-            commonPrefix = self.findPrefix(commonPrefix,string)
+    def longestCommonPrefix(self, m):
+        if not m: return ''
+				#since list of string will be sorted and retrieved min max by alphebetic order
+        s1 = min(m)
+        s2 = max(m)
 
-        return commonPrefix
-
-    def findPrefix(self,prefix, string):
-        while prefix is not "":
-            
-            if string.startswith(prefix):
-                
-                return prefix
-            else:
-                prefix = prefix[:-1]
-
-        return ""
+        for i, c in enumerate(s1):
+            if c != s2[i]:
+                return s1[:i] #stop until hit the split index
+        return s1
