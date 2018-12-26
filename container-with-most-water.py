d2f5888ccc3cc731f577f1ab32db9d9381d963a2
Siyuan Gong <contentgong@gmail.com>
2018-12-25 21:02:25 -0500

    Update container-with-most-water.py

--- container-with-most-water.py
+++ container-with-most-water.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        # https://leetcode.com/articles/container-with-most-water/#approach-2-two-pointer-approach
-        l, r = 0, len(height) - 1
-        max_vol = 0
-        while l < r:
-            max_vol = max(min(height[l], height[r]) * (r - l), max_vol)
-            if height[l] < height[r]:
-                l += 1
+        area, left, right, mx = 0, 0, len(height) - 1, max(height)
+        while left < right:
+            if mx * (right - left) < area: break
+            lv, rv = height[left], height[right]
+            width = right - left
+            if lv < rv:
+                if (width*lv) > area: area = width*lv
+                while height[left] <= lv:
+                    left += 1
             else:
-                r -= 1
-        return max_vol
+                if (width*rv) > area: area = width*rv
+                while height[right] <= rv and right:
+                    right -= 1
+        return area
