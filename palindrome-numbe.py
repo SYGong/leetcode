bfcaa3b79a0b0ae85916cab80b1775b31815ae4a
Siyuan Gong <contentgong@gmail.com>
2018-12-25 21:08:03 -0500

    Update palindrome-numbe.py

--- palindrome-numbe.py
+++ palindrome-numbe.py
@@ -1,21 +1,8 @@
-import math
-
 class Solution:
     def isPalindrome(self, x):
         """
         :type x: int
         :rtype: bool
         """
-        if x == 0:
-            return True
-        if x < 0 or x % 10 == 0:
-            return False
-        digits = int(math.log10(x)) + 1
-        rev_x = 0
-        for d in range(digits // 2):
-            rev_x *= 10 
-            rev_x += x % 10
-            x //= 10
-        if digits % 2 == 1:
-            x //= 10
-        return x == rev_x
+        testing = str(x)
+        return testing[::-1] == str(x)
