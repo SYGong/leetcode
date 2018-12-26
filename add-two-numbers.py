23d6681476d24a7f1dba1991d23583df4b47a58e
Siyuan Gong <contentgong@gmail.com>
2018-12-25 21:54:46 -0500

    Update add-two-numbers.py
    This is faster and comsume less space by override l1
    it do not need to eval 0 again and again

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -1,9 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def addTwoNumbers(self, l1, l2):
         """
@@ -11,15 +5,27 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        eval_ = lambda l: 0 if l is None else l.val
-        next_ = lambda l: None if l is None else l.next
-        curr = dummy_head = ListNode(0)  
-        carry = 0 # Initiated to carry from one iteration to another
-        while l1 or l2 or carry:
-            sum_ = eval_(l1) + eval_(l2) + carry
-            curr.next = ListNode(sum_ % 10)
-            curr = curr.next
-            l1 = next_(l1)
-            l2 = next_(l2)
-            carry = sum_ // 10
-        return dummy_head.next
+        dummy = ListNode(0)
+        dummy.next = l1
+        pre = dummy
+        carry = 0
+        while l1 and l2:
+            carry += l1.val+l2.val
+            l1.val = carry % 10
+            carry = carry // 10
+            l1 = l1.next
+            l2 = l2.next
+            pre = pre.next
+        if l1:
+            pre.next = l1
+        elif l2:
+            pre.next = l2
+        while pre and pre.next:
+            carry += pre.next.val
+            pre.next.val = carry % 10
+            carry = carry // 10
+            pre = pre.next
+        if carry:
+            pre.next = ListNode(carry)
+        return dummy.next
+            
