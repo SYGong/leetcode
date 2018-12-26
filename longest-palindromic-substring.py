88660246190c79e656a0d3b11404d6a281b35e4f
Siyuan Gong <contentgong@gmail.com>
2018-12-25 21:54:56 -0500

    Update longest-palindromic-substring.py

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -4,22 +4,27 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        max_left, max_len = 0, 0
-
-        def expand_to(ex_l, ex_r):
-            # left/right pointer
-            if (ex_l >= 0 
-                    and ex_r < len(s)                    
-                    and s[ex_l] == s[ex_r]):
-                expand_to(ex_l - 1, ex_r + 1)
-            else:
-                pal_len = ex_r - ex_l - 1
-                nonlocal max_left, max_len
-                if max_len < pal_len:
-                    max_left = ex_l + 1
-                    max_len = pal_len
-
-        for i in range(len(s)):
-            expand_to(i, i)
-            expand_to(i, i + 1)
-        return s[max_left : max_left + max_len]
+        if not s:
+            return ""
+        
+        if s == s[::-1]:
+            return s
+        
+        start = 0
+        max_length = 1
+        
+        for i in range(2, len(s) + 1):
+            if i - max_length - 2 >= 0:
+                temp = s[i - max_length - 2: i]
+                if temp == temp[::-1]:
+                    start = i - max_length - 2
+                    max_length += 2
+                    continue
+            
+            if i - max_length - 1 >= 0:
+                temp = s[i - max_length - 1: i]
+                if temp == temp[::-1]:
+                    start = i - max_length - 1
+                    max_length += 1
+                    
+        return s[start: start + max_length]
