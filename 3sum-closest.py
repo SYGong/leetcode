68e6efca73f5405b143da149a73cb216c4b5050f
Siyuan Gong <contentgong@gmail.com>
2018-12-28 14:12:30 -0500

    Update 3sum-closest.py

--- 3sum-closest.py
+++ 3sum-closest.py
@@ -1,6 +1,5 @@
 from bisect import bisect, bisect_left
 
-
 class Solution:
     def threeSumClosest(self, nums, target):
         """
@@ -9,42 +8,50 @@ class Solution:
         :rtype: int
         """
         nums = sorted(nums)
-        cumu_max = [0, nums[-1], nums[-1] + nums[-2]]
+
+        # Let top[i] be the sum
+        # of largest i numbers.
+        top = [
+            0,
+            nums[-1],
+            nums[-1] + nums[-2]
+        ]
+
         min_diff = float('inf')
-        three_sum = None
+        three_sum = 0
 
-        # Denotes the least one of num_ints (1,2,3...)
-        # integers by x, this function return
-        # smallest/largest index of x.
-        def lst(sum_, num_ints, lo=0):
-            nonlocal cumu_max, nums
-            num_ints_remain = num_ints - 1
-            most_i = bisect(
-                nums, sum_ // num_ints,
-                lo, len(nums) - num_ints)
-            least_v = sum_ - cumu_max[num_ints_remain]
-            least_i = bisect_left(
-                nums, least_v, 
-                lo, len(nums) - num_ints) - 1
+        # Find range of the least number in
+        # curr_n (0, 1, 2 or 3) numbers that
+        # sum up to curr_target, then find
+        # range of 2nd least number and so on
+        # by recursion. 
+        def closest(curr_target, curr_n, lo=0):
+            nonlocal nums, top, target, \
+                     min_diff, three_sum
 
-            least_i = max(least_i, lo)
-            return least_i, most_i+1
+            if curr_n == 0:
+                if abs(curr_target) < min_diff:
+                    min_diff = abs(curr_target)
+                    three_sum = target - curr_target
+                return
+            
+            next_n = curr_n - 1
+            max_i = len(nums) - curr_n
+            max_i = bisect(
+                nums, curr_target // curr_n,
+                lo, max_i)
+            min_i = bisect_left(
+                nums, curr_target - top[next_n],
+                lo, max_i) - 1
+            min_i = max(min_i, lo)
 
-        # Consider smallest number in triplets
-        a0, a1 = lst(target, 3)
-        for i in range(a0, a1):
-            two_sum = target - nums[i]
-            b0, b1 = lst(two_sum, 2, i + 1)
-            for j in range(b0, b1):
-                complement = two_sum - nums[j]
-                c0, c1 = lst(complement, 1, j + 1)
-                for w in nums[c0:c1]:
-                    diff = abs(complement - w)
-                    if diff == 0:
-                        return nums[i] + nums[j] + w
-                    elif diff < min_diff:
-                        min_diff = diff
-                        three_sum = nums[i] + nums[j] + w
+            for i in range(min_i, max_i + 1): 
+                if min_diff == 0:
+                    return
+                next_target = curr_target - nums[i]
+                closest(next_target, next_n, i + 1)
+
+        closest(target, 3)
         return three_sum
 
 
