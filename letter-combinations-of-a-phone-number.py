ee9fff32e78a3963e4f9dda11ea97fe82e1d886d
Siyuan Gong <contentgong@gmail.com>
2018-12-30 00:07:18 -0500

    Update letter-combinations-of-a-phone-number.py

--- letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number.py
@@ -1,18 +1,27 @@
 class Solution:
+    DIGITS_MAP = {
+        '2': "abc",
+        '3': "def",
+        '4': "ghi",
+        '5': "jkl",
+        '6': "mno",
+        '7': "pqrs",
+        '8': "tuv",
+        '9': "wxyz"
+    }
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
-        m ={'2':'abc', '3':'def', '4':'ghi', '5':'jkl',
-            '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
-        
         if len(digits) == 0:
             return []
         
-        res = [""]
-        
-        for d in digits:
-            res = [i+j for i in res for j in m[d]]
-            
-        return res
+        if len(digits) == 1:
+            return [letter for letter in self.DIGITS_MAP[digits[0]]]
+
+        combos = []
+        for option in self.letterCombinations(digits[1:]):
+            for letter in self.DIGITS_MAP[digits[0]]:
+                combos.append(letter+option)
+        return combos
