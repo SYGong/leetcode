d999c69a92ae6c47fdc67e8b1d42d61242a6d410
Siyuan Gong <contentgong@gmail.com>
2019-01-01 18:51:57 -0500

    Update remove-nth-node-from-end-of-list.py

--- remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list.py
@@ -15,10 +15,16 @@ class Solution:
         curr_node = head
         while curr_node.next:
             curr_node = curr_node.next
+            # if the list is long enough, curr_node is n node ahead from Np1th_from_curr and when curr_node.next is None, the curr_node is the end, let Np1.next = Np1.next.next
+            # the problem is, Np1 may not exist
             if n > 0:
                 n -= 1
             else:
                 Np1th_from_curr = Np1th_from_curr.next
-        Np1th_from_curr.next = Np1th_from_curr.next.next
+        if n > 0:
+            return None
+        else:
+            Np1th_from_curr.next =  Np1th_from_curr.next.next
+            return head
         
             
