b2c224c3df1af144cbe6db9f53865fbf6e341f85
Siyuan Gong <contentgong@gmail.com>
2019-04-05 14:18:46 -0400

    Update binary-tree-preorder-traversal.py

--- binary-tree-preorder-traversal.py
+++ binary-tree-preorder-traversal.py
@@ -1,15 +1,8 @@
 class Solution:
     def preorderTraversal(self, root: 'TreeNode') -> 'List[int]':
-        res = []
-        stack = []
-        cur = root
-        
-        while cur or len(stack) > 0:
-            if cur:
-                res.append(cur.val)
-                stack.append(cur)
-                cur = cur.left
-            else:
-                node = stack.pop()
-                cur = node.right
-        return res
+        vals = []
+        if root:
+            vals.append(root.val)
+            vals += self.preorderTraversal(root.left)
+            vals += self.preorderTraversal(root.right)
+        return vals
